11848eeb92a76f638c1298a3227269e203bd1adc
KUMARI NEHA <92735311+200221192@users.noreply.github.com>
2024-07-30 00:49:53 +0530

    Add files via upload

---
+++ JavaLibraryManagementSystem/src/javalibrarymanagementsystem/JavaLibraryManagementSystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javalibrarymanagementsystem;
+
+/**
+ *
+ * @author hp
+ */
+public class JavaLibraryManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
